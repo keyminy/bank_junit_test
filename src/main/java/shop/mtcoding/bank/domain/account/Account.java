06c4df8437f98614d20ef1b137b3b02b7a3bf82c
keyminy <imanaifs@gmail.com>
2023-10-07 19:16:19 +0900

    Account엔티티 생성

--- src/main/java/shop/mtcoding/bank/domain/account/Account.java
+++ src/main/java/shop/mtcoding/bank/domain/account/Account.java
@@ -1,15 +1,15 @@
-package shop.mtcoding.bank.domain.user;
+package shop.mtcoding.bank.domain.account;
 
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.CreatedDate;
@@ -19,28 +19,29 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import shop.mtcoding.bank.domain.user.User;
 
 @NoArgsConstructor //Full constructor가 생기면 꼭 넣어주자
 //스프링이 User객체를 생성할 때, 빈 생성자로 new를 하기때문.
 @Getter
 @EntityListeners(AuditingEntityListener.class)
-@Table(name="user_tb")
+@Table(name="account_tb")
 @Entity
-public class User {
+public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true,nullable = false,length = 20)
-    private String username;
-    @Column(nullable = false,length = 60) //패스워드 인코딩(BCrypt) 하면 늘어나므로 60자
-    private String password;
-    @Column(nullable = false,length = 20)
-    private String email;
-    @Column(nullable = false,length = 20)
-    private String fullname;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable=false)
-    private UserEnum role; // ADMIN,CUSTOMER
+    private Long number; //계좌번호
+    @Column(nullable = false,length = 4)
+    private Long password; //계좌비번
+    @Column(nullable = false)
+    private Long balance; //잔액(기본값 : 1000원)
+
+    //항상 ORM에서 FK의 주인은 Many Entity쪽이다.(FK는 account가 가짐)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user; //설정을 안해주면 기본값으로, 'user_id'형식으로 만들어짐
 
     @CreatedDate //Insert시 자동으로 들어감
     @Column(nullable=false)
@@ -51,15 +52,16 @@ public class User {
     private LocalDateTime updatedAt;
 
     @Builder
-    public User(Long id, String username, String password, String email, String fullname, UserEnum role,
-            LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Account(Long id, Long number, Long password, Long balance, User user, LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
         this.id = id;
-        this.username = username;
+        this.number = number;
         this.password = password;
-        this.email = email;
-        this.fullname = fullname;
-        this.role = role;
+        this.balance = balance;
+        this.user = user;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+
+    
 }
